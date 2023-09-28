45b119a9314d234a82f3a0bfc6d8a674eb4e9f51
Devesh Kumar <contact@devesh.tech>
2023-09-28 15:03:21 +0530

    chore: Log functions

---
+++ src/lib/log.ts
@@ -0,0 +1,11 @@
+export const log = (str: string) => {
+	console.log(str);
+};
+
+export const warn = (str: string) => {
+	console.warn(str);
+};
+
+export const error = (str: string) => {
+	console.error(str);
+};
